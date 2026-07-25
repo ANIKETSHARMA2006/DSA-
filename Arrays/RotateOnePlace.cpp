18e0a74592b28a2ac94a3488a0517b8fa310cd3d
NHGamer <aniket94sharma56@gmail.com>
2026-07-26 01:20:55 +0530

    add RotateOnePlace

---
+++ Arrays/RotateOnePlace.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void RotateOnePlace(vector<int> &nums){
+        int temp=nums[0];
+        for(int i=1;i<nums.size();i++){
+            nums[i-1]=nums[i];
+        }
+        nums[nums.size()-1]=temp;
+    }
+
+int main(){
+    vector<int> nums={1,2,3,4,5};
+    RotateOnePlace(nums);
+    for (int i = 0; i < nums.size(); i++){
+        cout<< nums[i];
+    }
+    
+return 0;
+}
